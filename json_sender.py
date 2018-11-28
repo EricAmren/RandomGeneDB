d5abcefe33c237e54f0dc56ff1b3b93ba5c59448
amren <eric.cumunel@gmail.com>
2018-11-28 14:33:36 +0100

    json sender with pythonanywhere

--- json_sender.py
+++ json_sender.py
@@ -1,5 +1,5 @@
 import requests
-res = requests.post('http://127.0.0.1:5000/api/Genes/?offset=100', json={"mytext":"lalala"})
+res = requests.post('http://amren.pythonanywhere.com/api/Genes/?offset=100', json={"mytext":"lalala"})
 if res.ok:
     print(res.json())
 else :
